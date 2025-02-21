4c6eaeede0d6ad5cb919cce6c50c7c5016930229
aswinihari <aswinihari329@gmail.com>
2025-02-21 11:16:41 +0530

    third commit

---
+++ Pratice_3.java
@@ -0,0 +1,11 @@
+class Pratice_3 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Once upon a time, in a quiet pond with tall plants and pretty flowers around it, a mother duck waited excitedly for her eggs to hatch. Finally, the day came when one egg cracked open, and out came a tiny, grayish duckling.");
+	    System.out.println("Even though the mother duck loved all her babies");
+		System.out.println("she noticed that this little one looked different from the rest.");
+	
+	
+	}
+}
